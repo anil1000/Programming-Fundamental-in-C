19f23ef51ead2db3e0aef6d8f8153476eb8dd09c
anil1000 <anil.paudel101@gmail.com>
2016-11-16 10:59:24 +0545

    Add files via upload

---
+++ sum of two no.cpp
@@ -0,0 +1,25 @@
+/* Student Name :Anil Paudel
+   Subject:- Programming Fundamental
+   Roll No. :- 02
+   Lab Sheet :- 02
+   Program :- Write a C program Print sum of two numbers,asked from users.
+   Date:- November 16,2016
+ */
+
+  #include<stdio.h>
+  #include<conio.h>
+    int main ()
+    {
+    int a,b,sum ;
+
+    printf("Enter the First no.\n");
+    scanf("%d",&a);
+    printf("Enter the Second no.\n");
+    scanf("%d",&b);
+    sum = a+b;
+    printf(" Sum of two number is:%d",sum);
+    getch ();
+    return 0;
+    }
+
+
