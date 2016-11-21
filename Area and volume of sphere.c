af98fe16a56ad14c4fbfc8182f5482a5032fa91e
anil1000 <anil.paudel101@gmail.com>
2016-11-21 12:35:17 +0545

    Add files via upload

---
+++ Area and volume of sphere.c
@@ -0,0 +1,23 @@
+/*
+   Student name : Anil Paudel
+   Roll: 02
+   Subject:Programming Fundamental
+   Program: WAP a C program to print area and perimeter of rectangle.
+   Lab sheet :04
+*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#define PI 3.14
+  int main()
+  {
+  float r,area,volume;
+  printf("Enter the radius:\n");
+  scanf("%f",&r);
+  area=4*PI*pow(r,2);
+  volume=4/3*PI*pow(r,3);
+  printf("The area of sphere is:%f\n",area);
+  printf("The volume of sphere is:%f",volume);
+     getch ();
+     return 0;
+  }
