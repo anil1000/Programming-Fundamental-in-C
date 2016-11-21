f864c0ceedcf876172fc72c7ef5fcf7de0367db3
anil1000 <anil.paudel101@gmail.com>
2016-11-21 11:11:45 +0545

    Add files via upload

--- area of triangle.c
+++ area of triangle.c
@@ -1,24 +1,26 @@
-/* Student Name :Anil Paudel
-   Subject:- Programming Fundamental
-   Roll No. :- 02
-   Lab Sheet :- 02
-   Program :- Write a C program Print area of triangle; base and height are asked from users.
-   Date:- November 16,2016
+/* Student Name: Anil Paudel
+   Roll No.: 02
+   Subject: Programming Fudamental
+   Labsheet: 04
+   Date: November 21,2016
+   Program: WAP a C program to find the area of a triangle
  */
-  #include<stdio.h>
-  #include<conio.h>
-    int main ()
-    {
-    float b,h,area=0;
-
-    printf("Enter the base:\n");
-    scanf("%f",&b);
-    printf("Enter the height:\n");
-    scanf("%f",&h);
-    area= 0.5*b*h ;
-    printf("The area of a triangle is:%f",area);
-
-    getch ();
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+  int main ()
+  {
+  float a,b,c,side,area,E;
+  printf("Enter the first side:\n");
+  scanf("%f",&a);
+  printf("Enter the second side:\n");
+  scanf("%f",&b);
+  printf("Enter the third side:\n");
+  scanf("%f",&c);
+  side=a+b+c;
+  E=side*(side-a)*(side-b)*(side-c);
+  area=pow(E,0.5);
+  printf("Area of triangle is:%f",area);
+     getch ();
      return 0;
-
-      }
+     }
