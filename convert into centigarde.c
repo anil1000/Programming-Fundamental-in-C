5f6fb2bf0d9c32d3a264aef1b973782f647eaaec
anil1000 <anil.paudel101@gmail.com>
2016-11-23 10:19:06 +0545

    Add files via upload

---
+++ convert into centigarde.c
@@ -0,0 +1,18 @@
+/* Student Name: Anil Paudel
+   Roll No.: 02
+   Labsheet: 04
+   Program : Write a program to read temperature in fahrenhit and convert it into centigrade.
+   Date : November 23,2016
+*/   
+#include<stdio.h>
+   int main ()
+      { 
+  float f,centigrade;
+printf("Enter the fahrenhit:\n");
+scanf("%f",&f);	  
+centigrade=(0.555)*(f-32);
+printf("centigrade:%f",centigrade);
+return 0;
+} 
+	   
+	   
---
+++ sum of three numbers.c
@@ -0,0 +1,23 @@
+/* Student Name: Anil Paudel
+   Roll No.: 02
+   Labsheet: 04
+   Program : Write a program to find sum of digits of three digits number.
+   Date : November 23,2016
+*/   
+#include<stdio.h>
+
+  int main ()
+  {
+  float a,b,c,sum=0;
+printf("Enter the first number:\n");
+scanf("%f",&a);
+printf("Enter the second number:\n");
+scanf("%f",&b);
+printf("Enter the third number:\n");
+scanf("%f",&c);
+sum=a+b+c;
+printf(" The sum of three number is:%f",sum);
+   
+   return 0;
+   }
+  
