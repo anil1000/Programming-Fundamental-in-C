a288515d025c4c13c1e79ab1844b139601543807
anil1000 <anil.paudel101@gmail.com>
2016-11-25 10:49:21 +0545

    Add files via upload

---
+++ tocheckoddoreven.c
@@ -0,0 +1,24 @@
+/* 
+Student Name: Anil Paudel
+   Roll no.: 02
+   Lab sheet: 06
+   Program: Write a c program to check the no. is odd or even.
+   Date: November 25,2016
+*/  
+#include<stdio.h>
+
+   int main()
+   {
+ int a;
+ printf("Enter the number:\n");
+ scanf("%d",&a);
+ 
+ if(a%2==0){
+ printf("the number is even:\n");
+ }
+ else 
+ {
+ printf("the number is odd:\n");
+ }
+   return 0;
+}
---
+++ tocheckthenumberispositiveornegative.c
@@ -0,0 +1,31 @@
+/* 
+Student Name: Anil Paudel
+   Roll no.: 02
+   Lab sheet: 06
+   Program: Write a c program to check the no. is positive or negative.
+   Date: November 25,2016
+*/  
+#include<stdio.h>
+
+int main()
+    {
+int a;
+printf("Enter the number:\n");
+scanf("%d",&a);
+
+if(a>0){
+printf(" The number is positive:\n");
+}
+else 
+{
+  printf("The number is negative:\n");
+}
+     return 0;
+     
+     
+     
+     
+     
+     
+ }
+  
