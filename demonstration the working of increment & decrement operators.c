3bf8dbe748d1a6623711ea216da78db6922d0f8c
anil1000 <anil.paudel101@gmail.com>
2016-12-02 12:54:05 +0545

    Add files via upload

---
+++ demonstration the working of increment & decrement operators.c
@@ -0,0 +1,23 @@
+/*
+ Student Name; Anil Paudel
+ Roll No.: 04
+ LabSheet: 09
+ Program: WAP to demonstration the working of increment & decrement operators
+ Date: December 2,2016
+*/
+ #include<stdio.h>
+ #include<conio.h>
+ int main()
+ {
+     int a;
+     printf("Enter the number:\n");
+     scanf("%d",&a);
+     printf(" post increment of operators is:%d\n",a++);
+     printf(" pre increment of operators is:%d\n",++a);
+     printf(" post decrement of operators is:%d\n",a--);
+     printf(" pre decrement of operators is:%d\n",--a);
+        getch ();
+        return 0;
+
+
+ }
