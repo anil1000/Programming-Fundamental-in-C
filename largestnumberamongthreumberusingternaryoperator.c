1f21e708737e19fb4fb9603def64adfb6702ec1a
anil1000 <anil.paudel101@gmail.com>
2016-12-09 11:04:59 +0545

    Add files via upload

---
+++ largestnumberamongthreumberusingternaryoperator.c
@@ -0,0 +1,20 @@
+/* Student Name:- Anil Paudel
+   ROll NO.:- 04
+   Program: WAP to print largest number among three number using ternary operator.
+   Lab Sheet:11
+   Date:Decmeber 9,2016
+*/   
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter the first number:\n");
+	scanf("%d",&a);
+	printf("Enter the second number:\n");
+	scanf("%d",&b);
+	printf("Enter the third number:\n");
+	scanf("%d",&c);
+	(a>=b && a>=c)?printf(" a is greater"):(b>=a && b>=c)?printf(" b is greater"):printf(" c is greater ");
+	
+	return 0;
+}		
